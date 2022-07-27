96c1a317af21145654da1922ff8639ab7d1e02d0
Eugene K <eugene.kobyakov@netfoundry.io>
2022-07-27 15:33:02 -0400

    lighten the build

--- deps.cmake
+++ deps.cmake
@@ -5,5 +5,6 @@ FetchContent_Declare(ziti
         GIT_REPOSITORY https://github.com/openziti/ziti-sdk-c
         GIT_TAG 0.29.3
         )
-
+set(ZITI_BUILD_PROGRAMS off)
+set(ZITI_BUILD_TESTS off)
 FetchContent_MakeAvailable(ziti)
