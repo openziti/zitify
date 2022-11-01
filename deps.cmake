ae60248e14c3bd323de206b6d5e6c9251d3e3680
Eugene K <eugene.kobyakov@netfoundry.io>
2022-11-01 11:47:27 -0400

    update ziti-sdk@v0.30.7

--- deps.cmake
+++ deps.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 
 FetchContent_Declare(ziti
         GIT_REPOSITORY https://github.com/openziti/ziti-sdk-c
-        GIT_TAG 0.29.3
+        GIT_TAG 0.30.7
         )
 set(ZITI_BUILD_PROGRAMS off)
 set(ZITI_BUILD_TESTS off)
