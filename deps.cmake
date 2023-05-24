e3385c68b07cb6a65596f7086dad64ddae890426
Eugene K <eugene.kobyakov@netfoundry.io>
2023-05-24 14:47:21 -0400

    use latest ziti-sdk

--- deps.cmake
+++ deps.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 
 FetchContent_Declare(ziti
         GIT_REPOSITORY https://github.com/openziti/ziti-sdk-c
-        GIT_TAG 0.31.4
+        GIT_TAG 0.32.6
         )
 set(ZITI_BUILD_PROGRAMS off)
 set(ZITI_BUILD_TESTS off)
