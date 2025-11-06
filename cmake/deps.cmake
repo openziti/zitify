0dae082920c9f6864696e75d03dd34e97a73ee89
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2025-11-06 11:06:41 -0500

    bump C-SDK

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 
 FetchContent_Declare(ziti
         GIT_REPOSITORY https://github.com/openziti/ziti-sdk-c
-        GIT_TAG 1.5.10
+        GIT_TAG 1.9.15
         )
 set(ZITI_BUILD_PROGRAMS off)
 set(ZITI_BUILD_TESTS off)
